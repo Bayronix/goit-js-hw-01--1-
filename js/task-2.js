59e908304c979d8de2db50cb29e62cb4494071bb
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-02-22 17:04:04 +0100

    commit

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,11 @@
 function getShippingMessage(country, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
-  const message = ` Shipping to ${country} will cost ${totalPrice} credits`;
-  console.log(message);
+  const message = `Shipping to ${country} will cost ${totalPrice} credits`;
+  return message;
 }
 
-getShippingMessage("Ukraine", 300, 20);
+const country = "Ukraine";
+const price = 300;
+const deliveryFee = 20;
+
+console.log(getShippingMessage(country, price, deliveryFee));
